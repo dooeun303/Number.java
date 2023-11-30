b6d08ceb0c91117224467775bf7e0c40ef586817
dooeun303 <silverju00@icloud.com>
2023-11-30 11:59:41 +0900

    Initial commit

---
+++ src/Number.java
@@ -0,0 +1,36 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Number {
+    public static void main(String[] args) {
+
+        //숫자
+        System.out.println(1 + 3); // 정수
+
+        System.out.println(1.3+2.2); // 실수
+
+        System.out.println(2*5); // 곱하기
+
+        System.out.println(6/2); // 나누기
+        System.out.println();
+
+
+        //문자
+        System.out.println('생'); // 문자는 '작은따옴표'
+
+        System.out.println("생생"); // 문자열은 "큰따옴표"
+
+        System.out.println("생생" + "정보통"); // 문자열은 더하기 가능
+
+        System.out.println("1" + "1"); // 문자열 출력
+        System.out.println();
+
+
+        // 이스케이프기법: 역슬래시(\)를 큰따옴표 앞에 쓰면 문법적 역할에서 도망쳐 문자로 인식
+        System.out.println("Eunju said, \"Welcome Programming World!\"");
+        System.out.println("hi \n"); // 줄바꿈
+        System.out.println("yeah");
+
+
+
+    }
+}
